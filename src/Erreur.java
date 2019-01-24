ce5961e785bd4f1ea4c59dd96c68d825a62d23ba
schwal180 <schwal180@b032-14.crio.dauphine.fr>
2019-01-24 13:23:47 +0100

    ajout classe erreur

---
+++ src/Erreur.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author schwal180
+ */
+public abstract class Erreur {
+	protected ArrayList<Chirurgie> listeChirurgiesErreur;
+	
+	public Erreur() {
+		this.listeChirurgiesErreur = new ArrayList<Chirurgie>();
+	}
+	
+	public Erreur(ArrayList<Chirurgie> listeChirurgiesErreur) {
+		this.listeChirurgiesErreur = listeChirurgiesErreur;	
+	}
+	
+	public void addChirurgie(Chirurgie c) {
+		this.listeChirurgiesErreur.add(c);
+	}
+	
+	public ArrayList<Chirurgie> getListeChirurgiesErreur() {
+		return listeChirurgiesErreur;
+	}
+	
+	public abstract String toString();
+}
---
+++ src/ErreurInterference.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author schwal180
+ */
+
+public class ErreurInterference  extends Erreur {
+
+	@Override
+	public String toString() {
+		String s = "Erreur Interférence : ";
+		for(Chirurgie c : listeChirurgiesErreur) {
+			s += "\n" + c.toString();
+		}
+		s+= "\n";
+		return s;
+	}
+}
