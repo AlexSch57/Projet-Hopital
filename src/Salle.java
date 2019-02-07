87aedb5401d828c4440774a0c4867348e5cbf0e6
kormli18 <kormli18@b042-28.crio.dauphine.fr>
2019-02-07 09:00:28 +0100

    Ajout enums

--- src/Salle.java
+++ src/Salle.java
@@ -8,13 +8,35 @@
  *
  * @author kormli18
  */
-public class Salle {
+public enum Salle {
 
-    private String nom;
 
-    public Salle(String nom) {
-        this.nom = nom;
+	BLOC_E1("BLOC-E1",1),
+	BLOC_E2("BLOC-E2",2),
+	BLOC_E3("BLOC-E3",3),
+	BLOC_U3("BLOC-U3",4),
+	BLOC_U4("BLOC-U4",5),
+	BLOC_U5("BLOC-U5",6),
+	BLOC_U6("BLOC-U6",7),
+	BLOC_U7("BLOC-U7",8),
+	BLOC_U8("BLOC-U8",9),
+	BLOC_U9("BLOC-U9",10),
+	BLOC_U10("BLOC-U10",11);
+	
+	private String nom;
+	private int value;
+    
+    Salle(String nom, int value)
+    {
+    	this.nom = nom;
+        this.value = value;
     }
+    
+    public int getValue()
+    {
+        return this.value;
+    }
+	 
 
     public String getNom() {
         return nom;
